86ac29d2323002b628f270ab12d53aa161d2a523
anildiwan007 <anildiwan@gmail.com>
2019-04-29 20:48:41 -0500

    Changing file names and structure

--- leaderboard/src/main/java/com/adinc/leaderboard/Solution.java
+++ leaderboard/src/main/java/com/adinc/leaderboard/Solution.java
@@ -1,4 +1,4 @@
-package com.adinc;
+package com.adinc.leaderboard;
 
 import java.io.*;
 import java.util.*;
--- sockmerchant/src/main/java/com/adinc/sockmerchant/App.java
+++ sockmerchant/src/main/java/com/adinc/sockmerchant/App.java
@@ -1,4 +1,4 @@
-package com.adinc;
+package com.adinc.sockmerchant;
 
 /**
  * Hello world!
