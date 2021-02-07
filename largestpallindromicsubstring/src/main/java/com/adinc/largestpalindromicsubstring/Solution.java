741942a2f98e2f4b71f1c44abe5ea68447d389ea
anildiwan007 <anildiwan@gmail.com>
2021-02-06 21:40:58 -0600

    Updating to return pallindromic string

--- largestpallindromicsubstring/src/main/java/com/adinc/largestpalindromicsubstring/Solution.java
+++ largestpallindromicsubstring/src/main/java/com/adinc/largestpalindromicsubstring/Solution.java
@@ -3,32 +3,40 @@ package com.adinc.largestpalindromicsubstring;
 public class Solution {
 
     public static void main(String[] args) {
-        final String input = args [0];
+        System.out.println("Longest pallindrome length is " + longestPalindrome(args[0]));
+    }
 
-        int longestPalindromeLength = 1;
+    public static String longestPalindrome(String s) {
+        final String input = s;
+
+        int longestPalindromeLength = 0;
+        String longestPallindrome = null;
 
         if (isPallindrome(input))
-            System.out.println("Longest pallindrome length is " + input.length());
+            return input;
 
         for (int i= 0; i < input.length(); i++) {
 
-            int tempLength = findLongestPallindromeLength(input.charAt(i), input.substring(i));
+            String temp = findLongestPallindrome(input.charAt(i), input.substring(i));
+            int tempLength = temp.length();
 
-            if (tempLength > longestPalindromeLength)
+            if (tempLength > longestPalindromeLength) {
+                longestPallindrome = temp;
                 longestPalindromeLength = tempLength;
+            }
         }
 
-        System.out.println("Longest pallindrome length is " + longestPalindromeLength);
+        return longestPallindrome;
     }
 
-    private static int findLongestPallindromeLength(char charAt, String input) {
+    private static String findLongestPallindrome(char charAt, String input) {
 
         String subStr = input.substring(input.indexOf(charAt), input.lastIndexOf(charAt) + 1);
 
         if (isPallindrome(subStr)) {
-            return subStr.length();
+            return subStr;
         } else
-            return findLongestPallindromeLength(charAt, subStr.substring(0, subStr.lastIndexOf(charAt) - 1));
+            return findLongestPallindrome(charAt, subStr.substring(0, subStr.lastIndexOf(charAt)));
 
     }
 
@@ -48,5 +56,4 @@ public class Solution {
 
 
     }
-
 }
