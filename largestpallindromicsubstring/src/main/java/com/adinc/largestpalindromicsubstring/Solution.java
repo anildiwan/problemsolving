79e1a2d888fbbc8e27f0b2d08b43493530ac314d
anildiwan007 <anildiwan@gmail.com>
2021-02-06 19:58:35 -0600

    Added code for longest length of a pallindrome

--- largestpallindromicsubstring/src/main/java/com/adinc/largestpalindromicsubstring/Solution.java
+++ largestpallindromicsubstring/src/main/java/com/adinc/largestpalindromicsubstring/Solution.java
@@ -5,24 +5,45 @@ public class Solution {
     public static void main(String[] args) {
         final String input = args [0];
 
-        String longestPalindrome = null;
+        int longestPalindromeLength = 1;
 
         if (isPallindrome(input))
-            System.out.println(input + ", " + input.length());
+            System.out.println("Longest pallindrome length is " + input.length());
 
-        for (int i = 0; i < input.length(); i++)
-            System.out.println(isPallindrome(null));
+        for (int i= 0; i < input.length(); i++) {
+
+            int tempLength = findLongestPallindromeLength(input.charAt(i), input.substring(i));
+
+            if (tempLength > longestPalindromeLength)
+                longestPalindromeLength = tempLength;
+        }
+
+        System.out.println("Longest pallindrome length is " + longestPalindromeLength);
     }
+
+    private static int findLongestPallindromeLength(char charAt, String input) {
+
+        String subStr = input.substring(input.indexOf(charAt), input.lastIndexOf(charAt) + 1);
+
+        if (isPallindrome(subStr)) {
+            return subStr.length();
+        } else
+            return findLongestPallindromeLength(charAt, subStr.substring(0, subStr.lastIndexOf(charAt) - 1));
+
+    }
+
 
     public static boolean isPallindrome (String input) {
 
-        if (input == null)
+        if (input == null) {
             return false;
+        }
 
         final StringBuilder sb = new StringBuilder(input);
 
-        if (sb.equals(sb.reverse()))
+        if (sb.toString().equals(sb.reverse().toString())) {
             return true;
+        }
         return false;
 
 
